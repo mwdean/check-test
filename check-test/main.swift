3749d08f81b9f41f0ae2fc9e1991f4e1a7a28e9a
Michael Dean <mdean@api-digital.com>
2017-10-10 21:26:57 -0500

    Initial Commit

---
+++ check-test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  check-test
+//
+//  Created by Michael Dean on 10/10/17.
+//  Copyright © 2017 Michael Dean. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
